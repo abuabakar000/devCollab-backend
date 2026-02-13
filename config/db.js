18706951bf74193fec6572faf722b2ed0e67e99c
abubakar000 <abubakarkhawaja74@gmail.com>
2026-02-14 01:37:33 +0500

    process issue

--- config/db.js
+++ config/db.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose"
 import dns from "dns"
 
+dns.setDefaultResultOrder("ipv4first")
+
 const connectDB = async () => {
+    if (mongoose.connection.readyState >= 1) return;
+
     try {
+        console.log("Connecting to MongoDB...");
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            serverSelectionTimeoutMS: 5000
+            serverSelectionTimeoutMS: 100000 // Increase timeout for initial connection
         })
         console.log(`MONGODB CONNECTED: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Database connection error: ${error.message}`);
-        // Do not call process.exit(1) in a serverless environment
     }
 }
 export default connectDB;
